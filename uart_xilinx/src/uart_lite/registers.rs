4bb415a0f3b20a6bd4cbb30d9fc8910efe742cc3
Zhouqi Jiang <luojia@hust.edu.cn>
2025-07-12 19:09:56 +0800

    feat(uart_xilinx): add documentation for struct Registers of uart_lite
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

--- uart_xilinx/src/uart_lite/registers.rs
+++ uart_xilinx/src/uart_lite/registers.rs
@@ -1,10 +1,17 @@
 use volatile_register::{RO, WO};
 
-/// # UART Registers
+/// UART Lite register block.
+///
+/// It consists of a control register, a status register, and a pair of
+/// transmit/receive FIFOs.
 #[repr(C)]
 pub struct Registers {
+    /// Receive data first-in first-out (FIFO) queue register.
     pub rx: RO<u32>,
+    /// Transmit data first-in first-out (FIFO) queue register.
     pub tx: WO<u32>,
+    /// Status register.
     pub stat: RO<u32>,
+    /// Control register.
     pub ctrl: WO<u32>,
 }
